a6ccd20c60872be5f7fb50340b0f5782afddb869
Carlos Artur <carlos.artur11@gmail.com>
2023-12-28 14:37:27 -0300

    feat: create initial logic to fetch the api

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import "./index.css";
+import { useEffect } from "react";
+import { getList } from "../../services/requests";
+import { ListCard } from "../../components";
+
 export const ListScreen = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [listData, setListData] = useState([]);
+
+  const loadListItens = async () => {
+    setLoading(true);
+    const result = await getList();
+    console.log({ result });
+    setListData(result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadListItens();
+  }, []);
+
+  return (
+    <div className="list-screen-container">
+      <div className="list-screen-content-container">
+        <div className="list-screen-header">
+          <img
+            className="logo-image"
+            src="/images/logo.png"
+            alt="supermarket-list-logo"
+          />
+          <h1>Lista Supermercado</h1>
+        </div>
+        <div className="list-screen-list-container">
+          {loading && <h3>Carregando...</h3>}
+          {listData && listData?.length > 0 ? (
+            listData.map((item) => <ListCard key={item._id} item={item} />)
+          ) : (
+            <h3>Sua lista está vazia, adicione um novo item.</h3>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
